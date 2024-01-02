9a8810716d7190ed584794c038f9e34ce36c4329
senorlocksmith <senorlocksmith@gmail.com>
2024-01-02 13:00:38 -0600

    Update setup.sh
    added more features and install menus

--- setup.sh
+++ setup.sh
@@ -31,12 +31,17 @@ EOF
 
 add_no_check_valid() {
     echo "Acquire::Check-Valid-Until false;" | tee -a /etc/apt/apt.conf.d/10-nocheckvalid
-    echo "10-nocheckvalid updated with Acquire::Check-Valid-Until false."
+    echo "Added Check-Valid-Until false."
 }
 
 apt_get_update() {
     apt-get update
     echo "apt-get update executed."
+}
+
+apt_get_upgrade() {
+    apt-get upgrade --force-yes
+    echo "System upgraded."
 }
 
 install_openssh_server() {
@@ -74,9 +79,30 @@ install_git() {
     echo "Git installed."
 }
 
-clone_pocketdesk() {
+install_pocketdesk() {
     git clone https://github.com/senorlocksmith/PocketDesk.git
-    echo "PocketDesk repository cloned."
+    cd PocketDesk
+
+    echo "Select the PocketDesk package to install:"
+    echo "1. PocketDESK.sh"
+    echo "2. PocketDESKlite.sh"
+    read -p "Enter your choice (1 or 2): " pocketdesk_choice
+
+    case $pocketdesk_choice in
+        1)
+            sudo chmod +x PocketDESK.sh
+            ./PocketDESK.sh
+            echo "PocketDESK installed."
+            ;;
+        2)
+            sudo chmod +x PocketDESKlite.sh
+            ./PocketDESKlite.sh
+            echo "PocketDESKlite installed."
+            ;;
+        *)
+            echo "Invalid option selected."
+            ;;
+    esac
 }
 
 clone_and_install_chip_battery_status() {
@@ -107,25 +133,58 @@ clone_and_run_chipbuild() {
 
 # Display menu options
 echo "Select the operation to perform:"
-echo "1. Perform all edits"
+echo "1. Setup PocketCHIP for 2024"
 echo "2. Edit sources.list"
 echo "3. Edit preferences"
 echo "4. Add Acquire::Check-Valid-Until false to 10-nocheckvalid"
 echo "5. Run apt-get update"
-echo "6. Install openssh-server"
-echo "7. Install debian-archive-keyring"
-echo "8. Install locales"
-echo "9. Generate en_US and en_US.UTF-8 locales"
-echo "10. Reconfigure locales"
-echo "11. Reconfigure timezone data"
-echo "12. Install Git"
-echo "13. Clone PocketDesk repository"
-echo "14. Clone and install CHIP Battery Status"
-echo "15. Clone PocketInstaller repository"
-echo "16. Clone and run chipbuild"
+echo "6. Run apt-get upgrade"
+echo "7. Install openssh-server"
+echo "8. Install debian-archive-keyring"
+echo "9. Install locales"
+echo "10. Generate en_US and en_US.UTF-8 locales"
+echo "11. Reconfigure locales"
+echo "12. Reconfigure timezone data"
+echo "13. Install Git"
+echo "14. Install PocketDesk"
+echo "15. Clone and install CHIP Battery Status"
+echo "16. Clone PocketInstaller repository"
+echo "17. Clone and run chipbuild"
 
 # Get user input
 read -p "Enter your choice (1-16): " choice
 
 # Execute the selected option
 case $choice in
+    1)
+        update_sources_list
+        update_preferences
+        add_no_check_valid
+        apt_get_update
+        install_openssh_server
+        install_debian_keyring
+        install_locales
+        generate_locales
+        reconfigure_locales
+        reconfigure_tzdata
+        install_git
+        install_pocketdesk
+        ;;
+    2) update_sources_list ;;
+    3) update_preferences ;;
+    4) add_no_check_valid ;;
+    5) apt_get_update ;;
+    6) apt_get_upgrade ;;
+    7) install_openssh_server ;;
+    8) install_debian_keyring ;;
+    9) install_locales ;;
+    10) generate_locales ;;
+    11) reconfigure_locales ;;
+    12) reconfigure_tzdata ;;
+    13) install_git ;;
+    14) install_pocketdesk ;;
+    15) clone_and_install_chip_battery_status ;;
+    16) clone_pocketinstaller ;;
+    17) clone_and_run_chipbuild ;;
+    *) echo "Invalid option selected." ;;
+esac
